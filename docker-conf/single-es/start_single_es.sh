266b240ac17fddb74997c490ee442507f8fe9a63
cocowool <cocowool@gmail.com>
2024-10-18 18:02:27 +0800

    modify code

--- docker-conf/single-es/start_single_es.sh
+++ docker-conf/single-es/start_single_es.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# 因为 -v 参数不支持相对路径，在本地具体环境中需修改本地文件路径地址
+
 docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" --rm --name es-single -p 9200:9200 -p 9300:9300 -v /Users/shiqiang/Projects/2024-projects/code-space/docker-conf/single-es/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -d docker.elastic.co/elasticsearch/elasticsearch:6.3.1
